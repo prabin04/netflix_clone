0ab5d062dfb41ade0610a00cff65d7428714d68d
Prabin Parajuli <prabin.b9@gmail.com>
2020-10-08 18:59:48 +0200

    Creating the Rows using CSS FlexBox

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">]
-        <h1>Hey Clever Programmer let's build Netflix Clone Front-end Today</h1>
-        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchTrending} />
-        <Row title="Trending Now" fetchUrl={requests.fetchActionMovies } />
+    <div className="App">
+      <h1>Hey!! let's build Netflix Clone Front-end</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchTrending}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,31 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row( {title, fetchUrl} ) {
-    const [movies, setMovies] = useState([]);
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-    //A snippet of code which runs based on a aspecific condition/variable
-    useEffect(() => {
-        //if [], run once when the row loads, and don't run again
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            //console.log(request);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    console.log(movies )
+  //A snippet of code which runs based on a aspecific condition/variable
+  useEffect(() => {
+    //if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      //console.log(request);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
+  //console.log(movies);
 
-    return (
-        <div>
-            {/* title */}
-            <h2>{title}</h2>
-            {/* container -> posters */}
-        </div>
-    )
+  return (
+    <div className="row">
+      {/* title */}
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+        {/* container -> posters */}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
