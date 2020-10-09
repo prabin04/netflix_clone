fac3793097516c60c81b2888b839c8bd696e0135
Prabin Parajuli <prabin.b9@gmail.com>
2020-10-09 16:50:59 +0200

    creating the navbar

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -4,13 +4,14 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
       {/* Nav */}
       {/* Banner */}
-
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
---
+++ netflix-clone/src/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://occ-0-513-38.1.nflxso.net/dnm/api/v6/0RO1pLmU93-gdXvuxd_iYjzPqkc/AAAABa4kM1cduOY7n8hxNofTNWcRprInP3dw0Fta7OC4gfEAcZe8AgHHS46VqJfPtxFuNG9I1aFQIkBJCDy5n4zzOBU.png?r=318"
+        alt="Netflix logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
