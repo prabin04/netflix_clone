40ef98c0950d37d6ae2d3355e995ad048f9ffb68
Prabin Parajuli <prabin.b9@gmail.com>
2020-10-09 17:47:31 +0200

    Creating Trailer Pop-ups on clicking the movie posters updated

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 import YouTube from "react-youtube";
-import MovieTrailer from 'movie-trailer';
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState([""]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   //A snippet of code which runs based on a aspecific condition/variable
   useEffect(() => {
@@ -31,18 +31,18 @@ function Row({ title, fetchUrl, isLargeRow }) {
     },
   };
 
-  const handleClick = () => {
-    if (trailerUrl){
-      setTrailerUrl('');
-    }else{
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
       movieTrailer(movie?.name || "")
-      .then(url=>{
-
-        const UrlParams = new URLSearchParams(new URL(url).search);
-        setTrailerUrl(UrlParams.get("v"));
-      }).catch((error) => console.log(error));
+        .then((url) => {
+          const UrlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(UrlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
     }
-  }
+  };
 
   //console.log(movies);
 
@@ -55,7 +55,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            onClick={( => handleClick(movie))}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
